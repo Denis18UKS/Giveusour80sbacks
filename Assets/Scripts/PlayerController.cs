938b48f81e71a31f0c6376212cb7680e5af03f7a
Denis18UKS <148848338+Denis18UKS@users.noreply.github.com>
2026-05-16 16:49:32 +0500

    Игрок с анимациями и готовым управлением

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,17 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     public float speed = 1f;
 
     private Rigidbody2D rb;
-    private Vector2 movement;
     private Animator animator;
+
+    private Vector2 movement;
+
+    // Последнее направление, чтобы idle оставался повернут
+    // в нужную сторону после остановки
+    private string lastDirection = "down";
 
     void Awake()
     {
@@ -16,6 +22,17 @@ public class PlayerController : MonoBehaviour
     }
 
     void Update()
+    {
+        HandleInput();
+        HandleAnimation();
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = movement * speed;
+    }
+
+    void HandleInput()
     {
         movement = Vector2.zero;
 
@@ -32,12 +49,73 @@ public class PlayerController : MonoBehaviour
             movement.x = 1;
 
         movement = movement.normalized;
-
-        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
-    void FixedUpdate()
+    void HandleAnimation()
     {
-        rb.linearVelocity = movement * speed;
+        // Персонаж движется
+        if (movement != Vector2.zero)
+        {
+            // Вверх (спиной к игроку)
+            if (movement.y > 0)
+            {
+                lastDirection = "up";
+                PlayAnimation("walk_bottom");
+            }
+
+            // Вниз (лицом к игроку)
+            else if (movement.y < 0)
+            {
+                lastDirection = "down";
+                PlayAnimation("walk");
+            }
+
+            // Влево
+            else if (movement.x < 0)
+            {
+                lastDirection = "left";
+                PlayAnimation("walk_left");
+            }
+
+            // Вправо
+            else if (movement.x > 0)
+            {
+                lastDirection = "right";
+                PlayAnimation("walk_right");
+            }
+        }
+
+        // Персонаж стоит
+        else
+        {
+            switch (lastDirection)
+            {
+                case "up":
+                    PlayAnimation("idle_bottom");
+                    break;
+
+                case "down":
+                    PlayAnimation("idle");
+                    break;
+
+                case "left":
+                    PlayAnimation("idle_left");
+                    break;
+
+                case "right":
+                    PlayAnimation("idle_right");
+                    break;
+            }
+        }
+    }
+
+    void PlayAnimation(string animationName)
+    {
+        AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (!state.IsName(animationName))
+        {
+            animator.Play(animationName);
+        }
     }
 }
